d020f75a62e49ffae7a9bfb7f8965979c7025e02
RMSuyama <rafaelmoreirasuyama@gmail.com>
2023-03-30 17:07:12 -0300

    te

--- src/components/Carroussel.vue
+++ src/components/Carroussel.vue
@@ -1,42 +1,46 @@
 <template>
   <div class="marquee">
-    <img src="../assets/imagem1.jpeg" width="25vw" height="60vh">
-    <img src="../assets/imagem2.jpeg" width="25vw" height="60vh">
-    <img src="../assets/imagem3.jpeg" width="25vw" height="60vh">
-    <img src="../assets/imagem5.jpeg" width="25vw" height="60vh">
-    <img src="../assets/imagem6.jpeg" width="25vw" height="60vh">
-    <img src="../assets/imagem7.jpeg" width="25vw" height="60vh">
-    <img src="../assets/imagem8.jpeg" width="25vw" height="60vh">
-    <img src="../assets/imagem9.jpeg" width="25vw" height="60vh">
-    <img src="../assets/imagem10.jpeg" width="25vw" height="60vh">
-    <img src="../assets/imagem4.jpeg" width="25vw" height="60vh">
+    <img src="../assets/imagem1.jpeg" style="width: 25vw; height: 60vh;">
+    <img src="../assets/imagem2.jpeg" style="width: 25vw; height: 60vh;">
+    <img src="../assets/imagem3.jpeg" style="width: 25vw; height: 60vh;">
+    <img src="../assets/imagem4.jpeg" style="width: 25vw; height: 60vh;">
+    <img src="../assets/imagem5.jpeg" style="width: 25vw; height: 60vh;">
+    <img src="../assets/imagem6.jpeg" style="width: 25vw; height: 60vh;">
+    <img src="../assets/imagem7.jpeg" style="width: 25vw; height: 60vh;">
+    <img src="../assets/imagem8.jpeg" style="width: 25vw; height: 60vh;">
+    <img src="../assets/imagem9.jpeg" style="width: 25vw; height: 60vh;">
+    <img src="../assets/imagem10.jpeg" style="width: 25vw; height: 60vh;">
+
+
   </div>
-</template>
+  </template>
+  
+  <style scoped>
+  .marquee {
+    width: 100%;
+    height: 70vh;
+    overflow: hidden;
+    white-space: nowrap;
 
-<style scoped>
-.marquee {
-  width: 100%;
-  height: 70vh;
-  overflow: hidden;
-  white-space: nowrap;
-}
-
-.marquee img {
-  display: inline-block;
-  width: 20vw;
-  height: 550px;
-  margin-right: 10px;
-  animation: marquee 90s linear infinite;
-  border: 2px solid white;
-  border-radius: 20px;
-}
-
-@keyframes marquee {
-  0% {
-    transform: translateX(-25vw);
   }
-  100% {
-    transform: translateX(-175vw);
+  
+  .marquee img {
+    display: inline-block;
+    width: 20vw;
+    height: 550px;
+    margin-right: 10px;
+    animation: marquee 90s linear infinite;
+    border: 2px solid white;
+    border-radius: 20px;
   }
-}
-</style>
+
+  @keyframes marquee {
+    0% {
+      transform: translateX(-25vw);
+    }
+    100% {
+      transform: translateX(-175vw);
+    }
+  }
+  </style>
+  
