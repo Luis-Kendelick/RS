fb3d1a4f3fff64fb75a61d3f0c17383aeb789fe6
RMSuyama <rafaelmoreirasuyama@gmail.com>
2023-03-30 16:52:54 -0300

    teste

--- src/components/Carroussel.vue
+++ src/components/Carroussel.vue
@@ -1,19 +1,20 @@
 <template>
-  <!-- <div class="marquee">
-    <img src="src\assets\WhatsApp Image 2023-03-29 at 14.58.02.jpeg" style="width: 25vw; height: 60vh;">
-    <img src="src\assets\WhatsApp Image 2023-03-29 at 14.58.03 (1).jpeg" style="width: 25vw; height: 60vh;">
-    <img src="src\assets\WhatsApp Image 2023-03-29 at 14.58.03 (2).jpeg" style="width: 25vw; height: 60vh;">
-    <img src="src\assets\WhatsApp Image 2023-03-29 at 14.58.04 (1).jpeg" style="width: 25vw; height: 60vh;">
-    <img src="src\assets\WhatsApp Image 2023-03-29 at 14.58.04 (2).jpeg" style="width: 25vw; height: 60vh;">
-    <img src="src\assets\WhatsApp Image 2023-03-29 at 14.58.04.jpeg" style="width: 25vw; height: 60vh;">
-    <img src="src\assets\WhatsApp Image 2023-03-29 at 14.58.03.jpeg" style="width: 25vw; height: 60vh;">
-    <img src="src\assets\WhatsApp Image 2023-03-29 at 16.06.09.jpeg" style="width: 25vw; height: 60vh;">
-    <img src="src\assets\WhatsApp Image 2023-03-29 at 16.06.10 (1).jpeg" style="width: 25vw; height: 60vh;">
-    <img src="src\assets\WhatsApp Image 2023-03-29 at 16.06.10.jpeg" style="width: 25vw; height: 60vh;">
+  <div class="marquee">
+    <img src="../assets/imagem1.jpeg" style="width: 25vw; height: 60vh;">
+    <img src="../assets/imagem2.jpeg" style="width: 25vw; height: 60vh;">
+    <img src="../assets/imagem3.jpeg" style="width: 25vw; height: 60vh;">
+    <img src="../assets/imagem5.jpeg" style="width: 25vw; height: 60vh;">
+    <img src="../assets/imagem6.jpeg" style="width: 25vw; height: 60vh;">
+    <img src="../assets/imagem7.jpeg" style="width: 25vw; height: 60vh;">
+    <img src="../assets/imagem8.jpeg" style="width: 25vw; height: 60vh;">
+    <img src="../assets/imagem9.jpeg" style="width: 25vw; height: 60vh;">
+    <img src="../assets/imagem10.jpeg" style="width: 25vw; height: 60vh;">
+    <img src="../assets/imagem4.jpeg" style="width: 25vw; height: 60vh;">
 
-  </div> -->
+
+  </div>
   </template>
-<!--   
+  
   <style scoped>
   .marquee {
     width: 100%;
@@ -29,7 +30,7 @@
     height: 550px;
     margin-right: 10px;
     animation: marquee 90s linear infinite;
-    border: 1px solid white;
+    border: 2px solid white;
     border-radius: 20px;
   }
 
@@ -42,4 +43,4 @@
     }
   }
   </style>
-   -->
+  
